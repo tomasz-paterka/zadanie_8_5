e5b14b604f2499c8a677c21e90071b5f2d587319
Tomasz Paterka <tomekpaterka@gmail.com>
2018-01-28 17:44:34 +0100

    initiated project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a = prompt('Enter "a" value'),
+    h = prompt('Enter "h" value'),
+    triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
